899ed302746a6caaeab25bee09ada18abc7de61c
Md Hafiz Al Foisal <alfoisla940700@gmail.com>
2022-10-01 12:07:46 +0600

    up

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -32,8 +32,8 @@ class ChatController extends Controller {
 
     public function chatDetails($user_id, $friend_id) {
         $chat = DB::table('chats')
-            ->where('user_id', $user_id)
-            ->where('friend_id', $friend_id)
+            ->where('send_by', $user_id)
+            ->where('send_by', $friend_id)
             ->orderBy('id', 'desc')
             ->paginate(50);
         $unread = DB::table('chats')
